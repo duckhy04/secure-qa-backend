8ab0c7365a1614f10cee1a492e2748c3a064bec8
duckhy04 <nguynduchuy12@gmail.com>
2024-11-27 19:02:59 +0700

    first commit

---
+++ src/main/java/org/example/secureqabackend/SecureQaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.secureqabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureQaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureQaBackendApplication.class, args);
+    }
+
+}
