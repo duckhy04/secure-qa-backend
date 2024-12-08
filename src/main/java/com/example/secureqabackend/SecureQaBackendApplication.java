754ac5ff29cc91d783e05db70f2d5114368ee0a6
duckhy04 <nguynduchuy12@gmail.com>
2024-12-08 20:25:52 +0700

    Fisrt commit

---
+++ src/main/java/com/example/secureqabackend/SecureQaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.secureqabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureQaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureQaBackendApplication.class, args);
+    }
+
+}
